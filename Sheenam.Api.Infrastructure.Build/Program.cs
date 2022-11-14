f34177430ba7bcf7356f988d2d4540c303f246af
jamshidbek001 <inomiddinovjamshidbek@gmail.com>
2022-11-14 07:54:10 +0100

    INFRA:Initialize Build Project

---
+++ Sheenam.Api.Infrastructure.Build/Program.cs
@@ -0,0 +1,73 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use Comfort and Peace
+//=================================
+
+using ADotNet.Clients;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks;
+using ADotNet.Models.Pipelines.GithubPipelines.DotNets.Tasks.SetupDotNetTaskV1s;
+
+var githubPipeline = new GithubPipeline
+{
+    Name = "Sheenam Build Pipeline",
+
+    OnEvents = new Events
+    {
+        PullRequest = new PullRequestEvent
+        {
+            Branches = new string[] { "master" }
+        },
+
+        Push = new PushEvent
+        {
+            Branches = new string[] { "master" }
+        }
+    },
+
+    Jobs = new Jobs
+    {
+        Build = new BuildJob
+        {
+            RunsOn = BuildMachines.Windows2022,
+
+            Steps = new List<GithubTask>
+            {
+                new CheckoutTaskV2
+                {
+                    Name = "Checking Out Code"
+                },
+
+                new SetupDotNetTaskV1
+                {
+                    Name = "Setting Up .Net",
+                    TargetDotNetVersion = new TargetDotNetVersion
+                    {
+                        DotNetVersion = "7.0.100"
+                    }
+                },
+
+                new RestoreTask
+                {
+                    Name = "Restoring Nuget Packages"
+                },
+
+                new DotNetBuildTask
+                {
+                    Name = "Building Project"
+                },
+
+                new TestTask
+                {
+                    Name = "Running Test"
+                }
+            }
+        }
+    }
+};
+
+var client = new ADotNetClient();
+
+client.SerializeAndWriteToFile(
+    adoPipeline: githubPipeline,
+    path: "../../../../.github/workflows/dotnet.yaml");
