2ef9de177a04edf99636b40876b9283e6ad28102
jamshidbek001 <inomiddinovjamshidbek@gmail.com>
2022-11-15 11:00:54 +0100

    BROKERS:Insert Guest

---
+++ Sheenam.Api/Brokers/Storages/IStorageBroker.Guests.cs
@@ -0,0 +1,15 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use Comfort and Peace
+//=================================
+
+using System.Threading.Tasks;
+using Sheenam.Api.Models.Foundations.Guests;
+
+namespace Sheenam.Api.Brokers.Storages
+{
+    public partial interface IStorageBroker
+    {
+        ValueTask<Guest> InsertGuestAsync(Guest guest);
+    }
+}
