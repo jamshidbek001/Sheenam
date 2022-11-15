ff6ac6c41a82cfe8b9b6d64d6557ea04aeffb6cb
jamshidbek001 <inomiddinovjamshidbek@gmail.com>
2022-11-15 10:22:21 +0100

    DATA:Model And Migrate Guests

--- Sheenam.Api/Startup.cs
+++ Sheenam.Api/Startup.cs
@@ -9,6 +9,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using Sheenam.Api.Brokers.Storages;
 
 namespace Sheenam.Api
 {
@@ -28,6 +29,7 @@ namespace Sheenam.Api
             };
 
             services.AddControllers();
+            services.AddDbContext<StorageBroker>();
 
             services.AddSwaggerGen(options =>
             {
