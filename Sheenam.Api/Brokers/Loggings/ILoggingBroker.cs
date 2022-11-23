739b6153026ef47519e89455f4304a48c4e41630
jamshidbek001 <inomiddinovjamshidbek@gmail.com>
2022-11-23 16:29:39 +0100

    CONFIG: Register Logging Broker

--- Sheenam.Api/Brokers/Loggings/ILoggingBroker.cs
+++ Sheenam.Api/Brokers/Loggings/ILoggingBroker.cs
@@ -1,6 +1,15 @@
-﻿namespace Sheenam.Api.Brokers.Loggings
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Peace
+//=================================
+
+using System;
+
+namespace Sheenam.Api.Brokers.Loggings
 {
     public interface ILoggingBroker
     {
+        void LogError(Exception exception);
+        void LogCritical(Exception exception);
     }
 }
