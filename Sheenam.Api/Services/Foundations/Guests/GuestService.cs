6ec7bb7a144b4946faeca82d9f37590ffb7e2e55
jamshidbek001 <inomiddinovjamshidbek@gmail.com>
2022-11-28 09:31:36 +0100

    ShouldAddGuestAsync -> FAIL

---
+++ Sheenam.Api/Services/Foundations/Guests/GuestService.cs
@@ -0,0 +1,23 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Peace
+//=================================
+
+using System;
+using System.Threading.Tasks;
+using Sheenam.Api.Brokers.Storages;
+using Sheenam.Api.Models.Foundations.Guests;
+
+namespace Sheenam.Api.Services.Foundations.Guests
+{
+    public class GuestService : IGuestService
+    {
+        private readonly IStorageBroker storageBroker;
+
+        public GuestService(IStorageBroker storageBroker) =>
+            this.storageBroker = storageBroker;
+
+        public ValueTask<Guest> AddGuestAsync(Guest guest) =>
+            throw new NotImplementedException();
+    }
+}
