8a9d81884aa2d068e439a9c19334982acd88336d
jamshidbek001 <inomiddinovjamshidbek@gmail.com>
2022-11-28 10:29:47 +0100

    ShouldThrowValidationExceptionOnAddIfGuestIsNullAndLogItAsync -> WIP

---
+++ Sheenam.Api.Tests.Unit/Services/Foundations/Guests/GuestServiceTests.Validations.Add.cs
@@ -0,0 +1,28 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Peace
+//=================================
+
+using Sheenam.Api.Models.Foundations.Guests;
+using Xunit;
+
+namespace Sheenam.Api.Tests.Unit.Services.Foundations.Guests
+{
+    public partial class GuestServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnAddIfGuestIsNullAndLogItAsync()
+        {
+            // given
+            Guest nullGuest = null;
+
+
+            // when
+
+
+            // then
+
+
+        }
+    }
+}
