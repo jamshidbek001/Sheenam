ceb468af776e11152fb6c8307b8a11ee9fec44d7
jamshidbek001 <inomiddinovjamshidbek@gmail.com>
2022-11-28 09:56:38 +0100

    CODE RUB: Rename Variable

--- Sheenam.Api.Tests.Unit/Services/Foundations/Guests/GuestServiceTests.Logic.add.cs
+++ Sheenam.Api.Tests.Unit/Services/Foundations/Guests/GuestServiceTests.Logic.add.cs
@@ -19,12 +19,12 @@ namespace Sheenam.Api.Tests.Unit.Services.Foundations.Guests
             // given
             Guest randomGuest = CreateRandomGuest();
             Guest inputGuest = randomGuest;
-            Guest returningGuest = inputGuest;
-            Guest expectedGuest = returningGuest.DeepClone();
+            Guest storageGuest = inputGuest;
+            Guest expectedGuest = storageGuest.DeepClone();
 
             this.storageBrokerMock.Setup(broker =>
                 broker.InsertGuestAsync(inputGuest))
-                .ReturnsAsync(returningGuest);
+                .ReturnsAsync(storageGuest);
 
             // when
             Guest actualGuest =
