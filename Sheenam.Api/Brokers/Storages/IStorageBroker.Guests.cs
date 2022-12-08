5e1e2476a1a37ff96b24f3b72791a9274b251e5b
jamshidbek001 <inomiddinovjamshidbek@gmail.com>
2022-12-08 11:27:54 +0100

    BROKERS: Retrieve All Guests

--- Sheenam.Api/Brokers/Storages/IStorageBroker.Guests.cs
+++ Sheenam.Api/Brokers/Storages/IStorageBroker.Guests.cs
@@ -3,6 +3,7 @@
 // Free To Use To Find Comfort and Peace
 //=================================
 
+using System.Linq;
 using System.Threading.Tasks;
 using Sheenam.Api.Models.Foundations.Guests;
 
@@ -11,5 +12,6 @@ namespace Sheenam.Api.Brokers.Storages
     public partial interface IStorageBroker
     {
         ValueTask<Guest> InsertGuestAsync(Guest guest);
+        IQueryable<Guest> SelectAllGuests();
     }
 }
--- Sheenam.Api/Brokers/Storages/StorageBroker.cs
+++ Sheenam.Api/Brokers/Storages/StorageBroker.cs
@@ -3,6 +3,7 @@
 // Free To Use To Find Comfort and Peace
 //=================================
 
+using System.Linq;
 using System.Threading.Tasks;
 using EFxceptions;
 using Microsoft.EntityFrameworkCore;
@@ -28,6 +29,13 @@ namespace Sheenam.Api.Brokers.Storages
             await broker.SaveChangesAsync();
 
             return @object;
+        }
+
+        private  IQueryable<T> SelectAll<T>() where T : class
+        {
+            var broker = new StorageBroker(this.configuration);
+
+            return broker.Set<T>();
         }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
