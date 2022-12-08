a305f8fb33825d4934d53129038d37e3169c522c
jamshidbek001 <inomiddinovjamshidbek@gmail.com>
2022-12-08 10:08:52 +0100

    CODE RUB: Upgrade InsertGuest

--- Sheenam.Api/Brokers/Storages/StorageBroker.Guests.cs
+++ Sheenam.Api/Brokers/Storages/StorageBroker.Guests.cs
@@ -5,7 +5,6 @@
 
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.ChangeTracking;
 using Sheenam.Api.Models.Foundations.Guests;
 
 namespace Sheenam.Api.Brokers.Storages
@@ -14,16 +13,7 @@ namespace Sheenam.Api.Brokers.Storages
     {
         public DbSet<Guest> Guests { get; set; }
 
-        public async ValueTask<Guest> InsertGuestAsync(Guest guest)
-        {
-            using var broker = new StorageBroker(this.configuration);
-
-            EntityEntry<Guest> guestEntityEntry =
-                await broker.Guests.AddAsync(guest);
-
-            await broker.SaveChangesAsync();
-
-            return guestEntityEntry.Entity;
-        }
+        public async ValueTask<Guest> InsertGuestAsync(Guest guest) =>
+            await InsertAsync(guest);
     }
 }
