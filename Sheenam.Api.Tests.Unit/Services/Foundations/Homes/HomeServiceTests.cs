07e3a2397e4fc5df88aa3aa82d5d7cc2809c27da
jamshidbek001 <inomiddinovjamshidbek@gmail.com>
2022-12-09 09:00:29 +0100

    ShouldAddHomeAsync -> FAIL

---
+++ Sheenam.Api.Tests.Unit/Services/Foundations/Homes/HomeServiceTests.cs
@@ -0,0 +1,42 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Peace
+//=================================
+
+using Moq;
+using Sheenam.Api.Brokers.Loggings;
+using Sheenam.Api.Brokers.Storages;
+using Sheenam.Api.Models.Foundations.Homes;
+using Sheenam.Api.Services.Foundations.Homes;
+using Tynamix.ObjectFiller;
+
+namespace Sheenam.Api.Tests.Unit.Services.Foundations.Homes
+{
+    public partial class HomeServiceTests
+    {
+        private readonly Mock<IStorageBroker> storageBrokerMock;
+        private readonly Mock<ILoggingBroker> loggingBrokerMock;
+        private readonly IHomeService homeService;
+
+        public HomeServiceTests()
+        {
+            this.storageBrokerMock = new Mock<IStorageBroker>();
+            this.loggingBrokerMock = new Mock<ILoggingBroker>();
+
+            this.homeService = new HomeService(
+                storageBroker: storageBrokerMock.Object,
+                loggingBroker: loggingBrokerMock.Object);
+        }
+
+        private static Home CreateRandomHome() =>
+            CreateHomeFiller().Create();
+
+        private static Filler<Home> CreateHomeFiller()
+        {
+            var filler = new Filler<Home>();
+
+            return filler;
+        }
+
+    }
+}
---
+++ Sheenam.Api/Services/Foundations/Homes/HomeService.cs
@@ -0,0 +1,28 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Peace
+//=================================
+
+using System.Threading.Tasks;
+using Sheenam.Api.Brokers.Loggings;
+using Sheenam.Api.Brokers.Storages;
+using Sheenam.Api.Models.Foundations.Homes;
+
+namespace Sheenam.Api.Services.Foundations.Homes
+{
+    public class HomeService : IHomeService
+    {
+        private readonly IStorageBroker storageBroker;
+        private readonly ILoggingBroker loggingBroker;
+
+        public HomeService(IStorageBroker storageBroker,
+            ILoggingBroker loggingBroker)
+        {
+            this.storageBroker = storageBroker;
+            this.loggingBroker = loggingBroker;
+        }
+
+        public ValueTask<Home> AddHomeAsync(Home home) =>
+            throw new System.NotImplementedException();
+    }
+}
