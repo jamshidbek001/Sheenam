34fb0c052cd18b243cb848b83bf1d6a379807bcd
jamshidbek001 <inomiddinovjamshidbek@gmail.com>
2022-12-18 23:32:22 +0100

    EXPOSERS: Post Home

---
+++ Sheenam.Api/Controllers/HomesController.cs
@@ -0,0 +1,54 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Peace
+//=================================
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+using Sheenam.Api.Models.Foundations.Homes;
+using Sheenam.Api.Models.Foundations.Homes.Exceptions;
+using Sheenam.Api.Services.Foundations.Homes;
+
+namespace Sheenam.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomesController : RESTFulController
+    {
+        private readonly IHomeService homeService;
+
+        public HomesController(IHomeService homeService) =>
+            this.homeService = homeService;
+
+        [HttpPost]
+        public async ValueTask<ActionResult<Home>> PostHomeAsync(Home home)
+        {
+            try
+            {
+                return await this.homeService.AddHomeAsync(home);
+            }
+            catch (HomeValidationException homeValidationException)
+            {
+                return BadRequest(homeValidationException.InnerException);
+            }
+            catch (HomeDependencyValidationException homeDependencyValidationException)
+                when (homeDependencyValidationException.InnerException is AlreadyExistsHomeException)
+            {
+                return Conflict(homeDependencyValidationException.InnerException);
+            }
+            catch (HomeDependencyValidationException homeDependencyValidationException)
+            {
+                return BadRequest(homeDependencyValidationException.InnerException);
+            }
+            catch (HomeDependencyException homeDependencyException)
+            {
+                return InternalServerError(homeDependencyException.InnerException);
+            }
+            catch (HomeServiceException homeServiceException)
+            {
+                return InternalServerError(homeServiceException.InnerException);
+            }
+        }
+    }
+}
