90d91759ceb5965a078bfd25cbf73b70b3606d5f
jamshidbek001 <inomiddinovjamshidbek@gmail.com>
2022-12-19 13:38:29 +0100

    ShouldRetrieveGuestByIdAsync -> PASS

--- Sheenam.Api/Services/Foundations/Guests/GuestService.cs
+++ Sheenam.Api/Services/Foundations/Guests/GuestService.cs
@@ -35,7 +35,7 @@ namespace Sheenam.Api.Services.Foundations.Guests
         public IQueryable<Guest> RetrieveAllGuests() =>
         TryCatch(() => this.storageBroker.SelectAllGuests());
 
-        public ValueTask<Guest> RetrieveGuestByIdAsync(Guid guestId) =>
-            throw new NotImplementedException();
+        public async ValueTask<Guest> RetrieveGuestByIdAsync(Guid guestId) =>
+            await this.storageBroker.SelectGuestByIdAsync(guestId);
     }
 }
