c70284f726d69251fc4a61d2ad6a07708382ab06
inomiddinovjamshidbek@gmail.com <inomiddinovjamshidbek@gmail.com>
2022-12-25 19:29:58 +0300

    BROKERS: Add DateTimeBroker

---
+++ Sheenam.Api/Brokers/DateTimes/DateTimeBroker.cs
@@ -0,0 +1,15 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Peace
+//=================================
+
+using System;
+
+namespace Sheenam.Api.Brokers.DateTimes
+{
+    public class DateTimeBroker : IDateTimeBroker
+    {
+        public DateTimeOffset GetCurrentDateTime() =>
+            DateTimeOffset.UtcNow;
+    }
+}
---
+++ Sheenam.Api/Brokers/DateTimes/IDateTimeBroker.cs
@@ -0,0 +1,14 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Peace
+//=================================
+
+using System;
+
+namespace Sheenam.Api.Brokers.DateTimes
+{
+    public interface IDateTimeBroker
+    {
+        DateTimeOffset GetCurrentDateTime();
+    }
+}
