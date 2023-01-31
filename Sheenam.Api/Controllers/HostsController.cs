ab9b6e9dd0d73034804d071d770f36628c53f4ec
Jamshidbek Inomiddinov <inomiddinovjamshidbek71@gmail.com>
2023-01-31 14:34:59 +0100

    EXPOSERS: Post Host

---
+++ Sheenam.Api/Controllers/HostsController.cs
@@ -0,0 +1,54 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Peace
+//=================================
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using RESTFulSense.Controllers;
+using Sheenam.Api.Models.Foundations.Hosts;
+using Sheenam.Api.Models.Foundations.Hosts.Exceptions;
+using Sheenam.Api.Services.Foundations.Hosts;
+
+namespace Sheenam.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HostsController : RESTFulController
+    {
+        private readonly IHostService hostService;
+
+        public HostsController(IHostService hostService) =>
+            this.hostService = hostService;
+
+        [HttpPost]
+        public async ValueTask<ActionResult<Host>> PostHostAsync(Host host)
+        {
+            try
+            {
+                return await this.hostService.AddHostAsync(host);
+            }
+            catch (HostValidationException hostValidationException)
+            {
+                return BadRequest(hostValidationException.InnerException);
+            }
+            catch (HostDependencyValidationException hostDependencyValidationException)
+                when (hostDependencyValidationException.InnerException is AlreadyExistHostException)
+            {
+                return Conflict(hostDependencyValidationException.InnerException);
+            }
+            catch (HostDependencyValidationException hostDependencyValidationException)
+            {
+                return BadRequest(hostDependencyValidationException.InnerException);
+            }
+            catch (HostDependencyException hostDependencyException)
+            {
+                return InternalServerError(hostDependencyException.InnerException);
+            }
+            catch (HostServiceException hostServiceException)
+            {
+                return InternalServerError(hostServiceException.InnerException);
+            }
+        }
+    }
+}
