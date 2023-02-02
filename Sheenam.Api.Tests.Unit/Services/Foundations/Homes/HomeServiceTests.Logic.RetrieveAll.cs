f8f5d83c0543e466de1a6857dc2677551be75a33
Jamshidbek Inomiddinov <inomiddinovjamshidbek71@gmail.com>
2023-02-02 10:24:11 +0100

    ShouldRetriveAllHomes -> FAIL

---
+++ Sheenam.Api.Tests.Unit/Services/Foundations/Homes/HomeServiceTests.Logic.RetrieveAll.cs
@@ -0,0 +1,39 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Peace
+//=================================
+
+using FluentAssertions;
+using Moq;
+using Sheenam.Api.Models.Foundations.Homes;
+using Xunit;
+
+namespace Sheenam.Api.Tests.Unit.Services.Foundations.Homes
+{
+    public partial class HomeServiceTests
+    {
+        [Fact]
+        public void ShouldRetriveAllHomes()
+        {
+            // given
+            IQueryable<Home> randomHomes = CreateRandomHomes();
+            IQueryable<Home> storageHomes = randomHomes;
+            IQueryable<Home> expectedHomes = storageHomes;
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectAllHomes()).Returns(storageHomes);
+
+            // when
+            IQueryable<Home> actualHome = this.homeService.RetrieveAllHomes();
+
+            // then
+            actualHome.Should().BeEquivalentTo(expectedHomes);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectAllHomes(), Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
--- Sheenam.Api/Services/Foundations/Homes/IHomeService.cs
+++ Sheenam.Api/Services/Foundations/Homes/IHomeService.cs
@@ -3,6 +3,7 @@
 // Free To Use To Find Comfort and Peace
 //=================================
 
+using System.Linq;
 using System.Threading.Tasks;
 using Sheenam.Api.Models.Foundations.Homes;
 
@@ -11,5 +12,6 @@ namespace Sheenam.Api.Services.Foundations.Homes
     public interface IHomeService
     {
         ValueTask<Home> AddHomeAsync(Home home);
+        IQueryable<Home> RetrieveAllHomes();
     }
 }
