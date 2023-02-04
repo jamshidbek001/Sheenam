b6e91dad6e0eaec26154d8ce802cfe5393ef3d6c
Jamshidbek Inomiddinov <inomiddinovjamshidbek71@gmail.com>
2023-02-04 18:26:44 +0100

    ShouldModifyHomeAsync -> PASS

--- Sheenam.Api/Services/Foundations/Homes/HomeService.cs
+++ Sheenam.Api/Services/Foundations/Homes/HomeService.cs
@@ -48,7 +48,12 @@ namespace Sheenam.Api.Services.Foundations.Homes
             return maybeHome;
         });
 
-        public ValueTask<Home> ModifyHomeAsync(Home home) =>
-            throw new NotImplementedException();
+        public async ValueTask<Home> ModifyHomeAsync(Home home)
+        {
+            Home maybeHome =
+                await this.storageBroker.SelectHomeByIdAsync(home.Id);
+
+            return await this.storageBroker.UpdateHomeAsync(home);
+        }
     }
 }
