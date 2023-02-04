b73153032215602571e0af9acdd16b8c451e9261
Jamshidbek Inomiddinov <inomiddinovjamshidbek71@gmail.com>
2023-02-04 18:44:13 +0100

    ShouldThrowValidationExceptionOnModifyIfHomeIsNullAndLogItAsync -> FAIL

---
+++ Sheenam.Api.Tests.Unit/Services/Foundations/Homes/HomeServiceTests.Validations.Modify.cs
@@ -0,0 +1,48 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Peace
+//=================================
+
+using FluentAssertions;
+using Moq;
+using Sheenam.Api.Models.Foundations.Homes;
+using Sheenam.Api.Models.Foundations.Homes.Exceptions;
+using Xunit;
+
+namespace Sheenam.Api.Tests.Unit.Services.Foundations.Homes
+{
+    public partial class HomeServiceTests
+    {
+        [Fact]
+        public async Task ShouldThrowValidationExceptionOnModifyIfHomeIsNullAndLogItAsync()
+        {
+            // given
+            Home nullHome = null;
+            var nullHomeException = new NullHomeException();
+
+            var expectedHomeValidationException =
+                new HomeValidationException(nullHomeException);
+
+            // when
+            ValueTask<Home> modifyHomeTask =
+                 this.homeService.ModifyHomeAsync(nullHome);
+
+            HomeValidationException actualHomeValidationException =
+                await Assert.ThrowsAsync<HomeValidationException>(modifyHomeTask.AsTask);
+
+            // then
+            actualHomeValidationException.Should().BeEquivalentTo(
+                expectedHomeValidationException);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogError(It.Is(SameExceptionAs(
+                    expectedHomeValidationException))), Times.Once);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectHomeByIdAsync(It.IsAny<Guid>()), Times.Once);
+
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
