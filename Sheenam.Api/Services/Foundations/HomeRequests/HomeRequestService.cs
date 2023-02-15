07cb166c5477db26ed8d9f3ff8354133f0f52169
Jamshidbek Inomiddinov <inomiddinovjamshidbek71@gmail.com>
2023-02-15 15:55:58 +0100

    ShouldThrowValidationExceptionOnRemoveIfIdIsInvalidAndLogItAsync -> PASS

--- Sheenam.Api/Services/Foundations/HomeRequests/HomeRequestService.cs
+++ Sheenam.Api/Services/Foundations/HomeRequests/HomeRequestService.cs
@@ -66,12 +66,15 @@ namespace Sheenam.Api.Services.Foundations.HomeRequests
             return await this.storageBroker.UpdateHomeRequestAsync(homeRequest);
         });
 
-        public async ValueTask<HomeRequest> RemoveHomeRequestByIdAsync(Guid homeRequestId)
+        public ValueTask<HomeRequest> RemoveHomeRequestByIdAsync(Guid homeRequestId) =>
+        TryCatch(async () =>
         {
+            ValidateHomeRequestId(homeRequestId);
+
             HomeRequest someHomeRequest =
                 await this.storageBroker.SelectHomeRequestByIdAsync(homeRequestId);
 
             return await this.storageBroker.DeleteHomeRequestAsync(someHomeRequest);
-        }
+        });
     }
 }
