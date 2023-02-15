b1e4ace4e16cd32850fb199aad25b4b2fd28d7b2
Jamshidbek Inomiddinov <inomiddinovjamshidbek71@gmail.com>
2023-02-15 16:32:30 +0100

    ShouldThrowDependencyExceptionOnRemoveIfSqlErrorOccursAndLogItAsync -> PASS

---
+++ Sheenam.Api.Tests.Unit/Services/Foundations/HomeRequests/HomeRequestServiceTests.Exceptions.Remove.cs
@@ -0,0 +1,58 @@
+﻿//=================================
+// Copyright (c) Coalition of Good-Hearted Engineers
+// Free To Use To Find Comfort and Peace
+//=================================
+
+using FluentAssertions;
+using Microsoft.Data.SqlClient;
+using Moq;
+using Sheenam.Api.Models.Foundations.HomeRequests;
+using Sheenam.Api.Models.Foundations.HomeRequests.Exceptions;
+using Xunit;
+
+namespace Sheenam.Api.Tests.Unit.Services.Foundations.HomeRequests
+{
+    public partial class HomeRequestServiceTests
+    {
+        public ValueTask HomeReqeust { get; private set; }
+
+        [Fact]
+        public async Task ShouldThrowDependencyExceptionOnRemoveIfSqlErrorOccursAndLogItAsync()
+        {
+            // given
+            Guid someHomeRequestId = Guid.NewGuid();
+            SqlException sqlException = CreateSqlException();
+
+            var failedHomeRequestStorageException =
+                new FailedHomeRequestStorageException(sqlException);
+
+            var excpectedHomeRequestDependencyException =
+                new HomeRequestDependencyException(failedHomeRequestStorageException);
+
+            this.storageBrokerMock.Setup(broker =>
+                broker.SelectHomeRequestByIdAsync(It.IsAny<Guid>())).ThrowsAsync(sqlException);
+
+            // when
+            ValueTask<HomeRequest> removeHomeRequestTask =
+                this.homeRequestService.RemoveHomeRequestByIdAsync(someHomeRequestId);
+
+            HomeRequestDependencyException actualHomeRequestDependencyException =
+                await Assert.ThrowsAsync<HomeRequestDependencyException>(removeHomeRequestTask.AsTask);
+
+            // then
+            actualHomeRequestDependencyException.Should().BeEquivalentTo(
+                excpectedHomeRequestDependencyException);
+
+            this.storageBrokerMock.Verify(broker =>
+                broker.SelectHomeRequestByIdAsync(It.IsAny<Guid>()), Times.Once);
+
+            this.loggingBrokerMock.Verify(broker =>
+                broker.LogCritical(It.Is(SameExceptionAs(
+                    excpectedHomeRequestDependencyException))), Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+            this.loggingBrokerMock.VerifyNoOtherCalls();
+            this.dateTimeBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
